f23319a9e65299cb6bae396415ffd4099f9c0eab
LuizaMannes <lulusil23@gmail.com>
2026-06-25 17:31:05 -0300

    tirei import inutil

--- fliperama/src/test/java/com/brutebowling/ChamadoTeste.java
+++ fliperama/src/test/java/com/brutebowling/ChamadoTeste.java
@@ -1,11 +1,9 @@
 package com.brutebowling;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.time.LocalDate;
 import java.util.Date;
 
 import org.junit.jupiter.api.BeforeEach;
